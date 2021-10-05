fc2017c06441829c55145e5c70179fbd1ac907e9
rahmat_juniardi <rahmat9b23@gmail.com>
2021-10-05 20:54:20 +0700

    Materi Praktikum 1

---
+++ latihan1_download.php
@@ -0,0 +1,9 @@
+<?php
+$myDir = "E:/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank' download>$tmp</a><br>";
+}
+closedir($dir);
+?>
